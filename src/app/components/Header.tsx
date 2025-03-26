238a2a4a1456ccf8598c7893a3ea8bc5901a81b2
vincenzo <vincenzofarro01@gmail.com>
2025-03-26 22:41:35 +0100

    c1 - page structure

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+      <header>
+        <nav>
+          <a href="#hero">Home</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </header>
+    );
+  }
